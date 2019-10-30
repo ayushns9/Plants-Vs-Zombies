1ac96b30bdeff45b596af0342224771e08400cfe
ayushns9 <ayushg9716@gmail.com>
2019-10-30 18:06:14 +0530

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -33,6 +33,7 @@ public class Main extends Application {
         window.setScene(scene);
         window.show();
         Zombies z = new Zombies(pane);
+        z.move(pane);
     }
 
     public static void main(String[] args) {
