feaa3a33cb171b04c7393092b87a4f6d0b679dc0
ayushns9 <ayushg9716@gmail.com>
2019-10-30 15:06:10 +0530

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,33 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.StackedAreaChart;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        Scene scene  = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    public static void changeScene(String fxml, ActionEvent event) throws IOException {
+        Parent pane = FXMLLoader.load(Main.class.getResource(fxml));
+        Scene scene = new Scene(pane);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
